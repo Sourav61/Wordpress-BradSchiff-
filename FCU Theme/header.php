b66ade5981fd35bd0ba88b17bf359e6a2f0edc46
Sourav Pahwa <Sourav61pahwa@gmail.com>
2023-10-04 17:38:59 +0530

    Completed Till Section-5
    Completed Vid 13-19

--- FCU Theme/header.php
+++ FCU Theme/header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 
-<body>
-    <header class="site-header">
+<body <?php body_class() ?>>
+    <header class="site-header" style="margin-top:30px">
         <div class="container">
             <h1 class="school-logo-text float-left">
                 <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
@@ -15,14 +17,21 @@
                     aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
-                <nav class="main-navigation">
-                    <ul class="min-list group">
+                <nav class="main-navigation" style="display: flex">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'headerMenuLocation'
+                        )
+                    );
+                    ?>
+                    <!-- <ul class="min-list group">
                         <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                         <li><a href=" # ?>">Programs</a></li>
                         <li><a href="# ?>">Events</a></li>
                         <li><a href=" # ?>">Campuses</a></li>
                         <li><a href="# ?>">Blog</a></li>
-                    </ul>
+                    </ul> -->
                 </nav>
                 <div class=" site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
